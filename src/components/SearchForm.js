448dd414a92598915de81682ae725baec8a8444f
Ashley Dunham <xenocide21@gmail.com>
2019-11-15 13:19:01 -0700

    api call sends from search form (with error)

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,17 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios'
 
 export default function SearchForm({ onSearch }) {
 
     const [searchName, setSearchName] = useState([]);
-
+    useEffect(()=>{
+       axios
+           .get(`https://rick-api.herokuapp.com/api/character/${searchName}`)
+           .then( r => {
+               console.log(r.data.results);
+               setSearchName(r.data.results);
+           })
+    });
     const handleInputChange = (e) => {
         e.preventDefault();
         setSearchName(e.target.value)
