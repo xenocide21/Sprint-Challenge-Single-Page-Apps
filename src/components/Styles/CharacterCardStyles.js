3935e69357b42aba26eec6a555d5a7b1d110c85f
Ashley Dunham <xenocide21@gmail.com>
2019-11-16 01:37:36 -0700

    corrected a typo with named import

--- src/components/Styles/CharacterCardStyles.js
+++ src/components/Styles/CharacterCardStyles.js
@@ -1,7 +1,7 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import { makeStyles } from "@material-ui/core/styles/makeStyles";
 import {Paper} from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 
