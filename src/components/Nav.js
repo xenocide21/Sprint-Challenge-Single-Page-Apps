c953ea1818c2a3eb1ba81c1fd54f4e8b307d4f48
Ashley Dunham <xenocide21@gmail.com>
2019-11-16 01:23:29 -0700

    organized style imports to NavStylesImport.js to clean up Nav.js imports

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,7 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import HomeIcon from '@material-ui/icons/Home';
-import PersonIcon from '@material-ui/icons/Person';
-import LocationIcon from '@material-ui/icons/LocationCity';
+import { makeStyles, AppBar, Typography, Tab, Tabs, Box, HomeIcon, PersonIcon, LocationIcon } from "./NavStylesImport";
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
---
+++ src/components/NavStylesImport.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import HomeIcon from '@material-ui/icons/Home';
+import PersonIcon from '@material-ui/icons/Person';
+import LocationIcon from '@material-ui/icons/LocationCity';
+
+export { makeStyles , AppBar, Typography, Tab, Tabs, Box, HomeIcon, PersonIcon, LocationIcon }
